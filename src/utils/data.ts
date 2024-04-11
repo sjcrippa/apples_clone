fffcf22e61e29ea727a55fd2fb459bb7816d0ef4
Santiago Crippa <santicrippa@gmail.com>
2024-04-11 19:28:11 -0300

    structuring

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,13 +1,6 @@
 import hero from '/public/assets/images/hero.jpeg'
 
-/* import hmv from '/public/assets/videos/hero.mp4'
-import smallmv from '../assets/videos/smallHero.mp4'
-import highlightFirstmv from '../assets/videos/highlight-first.mp4'
-import highlightSectmv from '../assets/videos/hightlight-third.mp4'
-import highlightThirdmv from '../assets/videos/hightlight-sec.mp4'
-import highlightFourthmv from '../assets/videos/hightlight-fourth.mp4'
-import exploremv from '../assets/videos/explore.mp4'
-import framemv from '../assets/videos/frame.mp4' */
+// import hmv from '@/videos/explore.mp4'
 
 import apple from '/public/assets/images/apple.svg'
 import search from '/public/assets/images/search.svg'
@@ -29,14 +22,7 @@ import frame from '/public/assets/images/frame.png'
 
 export const heroImg = hero
 
-/* export const heroVideo = hmv
-export const smallHeroVideo = smallmv
-export const highlightFirstVideo = highlightFirstmv
-export const highlightSecondVideo = highlightSectmv
-export const highlightThirdVideo = highlightThirdmv
-export const highlightFourthVideo = highlightFourthmv
-export const exploreVideo = exploremv
-export const frameVideo = framemv */
+// export const heroVideo = hmv
 
 export const appleImg = apple
 export const searchImg = search
